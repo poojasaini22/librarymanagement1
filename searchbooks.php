68dd38824f66077e469f1526fce33daea44d0544
poojasaini22 <poojasaini96@gmail.com>
2018-12-29 23:41:27 +0530

    Update searchbooks.php

--- searchbooks.php
+++ searchbooks.php
@@ -3,7 +3,7 @@
 <html>
 <body bgcolor="87ceeb">
 <center><h2>Simple Library Management System</h2></center>
-<form action = "DisplayBooks.php" method="get">
+<form action = "displaybooks.php" method="get">
 <br>
 <center>Enter the title of the book to be searched :
 <input type="text" name="search" size="48">
